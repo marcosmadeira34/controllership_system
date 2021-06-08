7861a575fcc9b841d98f5ba5c0383b68d7e5c458
Marcos Madeira <mmadeirasilva5@gmail.com>
2021-06-08 15:02:17 -0300

    Alterado parse e root

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from genericpath import isfile
+# Libries import
+
 from os import listdir, path
 from os.path import isfile, join
 import time
-from xml.etree import ElementTree as ET
+from xml import etree
+import xml.etree.ElementTree as et
+
 
 list_xml = []
-def count_file():    
+def captura_xml():    
     path = input("Digte o caminho do arquivo: ")
     files = [f for f in listdir(path) if isfile(join(path, f))]
     list_xml.append(files)
     print("Aguarde...")
-    #time.sleep(4)
     print(len(files),"notas fiscais localizadas...")
-    time.sleep(4)
-count_file()
+    time.sleep(3.0)
+    directory = path    
+    list_files = []
+    files = [f for f in listdir(directory) if f.endswith('.xml')]
+    
+    for x in files:
+        list_files.append(x)
+    print(list_files)
 
-"""
-def extrair_info():
-    print("Carregando informações fiscais dos arquivos")
-    tree = ET.parse('')
-    root = tree.getroot()
-    print(root)
-extrair_info() """
+    mytree = et.parse(r'')
+    myroot = mytree.getroot()
+    print(myroot)
+    time.sleep(5.0)
+    print('aguarde.....')
+
+    for child in myroot.findall(' ide '):
+        cUF = child.find('cUF').text
+        cNF = child.get('cNF').text
+        print(cUF, cNF)
+
+captura_xml()
+
